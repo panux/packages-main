e41f37623887b475862a8e02ac4f1024057a2ef2
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-03 13:42:22 -0400

    add error propogation

--- buildpkg.sh
+++ buildpkg.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name build -v $(realpath .):/build -v $(realpath out):$(realpath $2) panux/package-builder /build/$1.pkgen /out
+docker run -it --rm --name build -v $(realpath .):/build -v $(realpath out):$(realpath $2) panux/package-builder /build/$1.pkgen /out || exit 1
