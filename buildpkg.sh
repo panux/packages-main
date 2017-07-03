b2405e539dbb6c4eb6b5fe2f524f773bdb40c136
Jaden Weiss <jadr2ddude@gmail.com>
2017-07-03 13:46:07 -0400

    fix output for buildpkg.sh

--- buildpkg.sh
+++ buildpkg.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name build -v $(realpath .):/build -v $(realpath out):$(realpath $2) panux/package-builder /build/$1.pkgen /out || exit 1
+docker run -it --rm --name build -v $(realpath .):/build -v $(realpath $2):/out panux/package-builder /build/$1.pkgen /out
