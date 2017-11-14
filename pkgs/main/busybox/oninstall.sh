0b559503b9983c4318e20453aad2b6671d64af9c
Jaden Weiss <jadr2ddude@gmail.com>
2017-11-13 22:55:33 -0500

    re-simplify oninstall scripts

--- pkgs/main/busybox/oninstall.sh
+++ pkgs/main/busybox/oninstall.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-cmd="$1"
-rootfs="$2"
-
-alt() {
-    if [ -z "$bootstrap" ]; then
-        lpkg-alt update "$1"
-    else
-        sh "$bootstrap/alternative.sh" update "$1"
-    fi
-}
-
 for i in $(busybox --list); do
-    alt $i
+    lpkg-alt update $i
 done
--- pkgs/main/wget/oninstall.sh
+++ pkgs/main/wget/oninstall.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-cmd="$1"
-rootfs="$2"
-
-alt() {
-    if [ -z "$bootstrap" ]; then
-        lpkg-alt update "$1"
-    else
-        sh "$bootstrap/alternative.sh" update "$1"
-    fi
-}
-
-alt wget
+lpkg-alt update wget
