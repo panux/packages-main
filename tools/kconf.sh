e6bdd27e0eb18b28a7f18447b4894df9a25cc85e
Jaden Weiss <jadr2ddude@gmail.com>
2018-01-04 08:38:20 -0500

    update kernel

--- tools/kconf.sh
+++ tools/kconf.sh
@@ -2,4 +2,4 @@
 
 docker pull panux/menuconf-docker
 
-docker run -it --rm -v $(realpath pkgs/kernel/linux/conf):/kernel panux/menuconf-docker 4.13.10 /kernel/$1.conf $1
+docker run -it --rm -v $(realpath pkgs/kernel/linux/conf):/kernel panux/menuconf-docker 4.14.11 /kernel/$1.conf $1
