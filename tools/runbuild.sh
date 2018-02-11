738bf3e92b75842fc0283deba8eded58966852b9
Jaden Weiss <jadr2ddude@gmail.com>
2018-02-11 18:52:20 -0500

    things

--- tools/runbuild.sh
+++ tools/runbuild.sh
@@ -8,6 +8,6 @@ sdir="$3"
 
 odir="$PWD/out/$arch"
 
-docker run -v $(realpath $sdir):/src -v $(realpath $odir):/out -v $(realpath tools):/tools "$img" /tools/cbuild.sh /src /root /out "$arch"
+docker run -it -v $(realpath $sdir):/src -v $(realpath $odir):/out -v $(realpath tools):/tools "$img" /tools/cbuild.sh /src /root /out "$arch"
 
 touch "$4"
