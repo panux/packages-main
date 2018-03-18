c207ee9cc42a1eb5c3b3a9673a2b1954469cc65e
Jaden Weiss <jadr2ddude@gmail.com>
2018-03-18 00:16:36 -0400

    fix up docker run command to handle path with spaces

--- tools/containerize.sh
+++ tools/containerize.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build tools -t panux/packages-main
-exec docker run --rm -it -e PMP=$(pwd) -v $(pwd):/root/packages-main -v /var/run/docker.sock:/var/run/docker.sock panux/packages-main "$@"
+exec docker run --rm -it -e PMP="$(pwd)" -v "$(pwd)":/root/packages-main -v /var/run/docker.sock:/var/run/docker.sock panux/packages-main "$@"
