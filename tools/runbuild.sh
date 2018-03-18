8255bd34b3015e97f9182a6dba3e20c72775b7b9
Jaden Weiss <jadr2ddude@gmail.com>
2018-03-18 00:17:07 -0400

    Fix output directory

--- tools/runbuild.sh
+++ tools/runbuild.sh
@@ -8,6 +8,6 @@ sdir="$3"
 
 odir="$PMP/out/$arch"
 
-docker run -it -v "$PMP/$sdir":/src -v "$PMP/$odir":/out -v "$PMP/tools":/tools "$img" /tools/cbuild.sh /src /root /out "$arch"
+docker run -it -v "$PMP/$sdir":/src -v "$odir":/out -v "$PMP/tools":/tools "$img" /tools/cbuild.sh /src /root /out "$arch"
 
 touch "$4"
