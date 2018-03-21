9138b555624aaa5e86e1192ead782a1b0425cb48
Jaden Weiss <jadr2ddude@gmail.com>
2018-03-20 22:53:09 -0400

    fix eudev oninstall to work for builder

---
+++ pkgs/main/eudev/oninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$1" != install ]; then
+  exit 0
+fi
+
+if [ ! -e "$2/etc/group" ]; then
+  touch "$2/etc/group"
+fi
+
+for g in tty dialout kmem input video audio lp disk cdrom tape; do
+  chroot "$2" addgroup -S $g;
+done
